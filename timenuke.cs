93c87dbaa03b6f4eacdb935540466822519bf979
Bill <72485122+dodochenpai@users.noreply.github.com>
2024-02-29 19:39:33 -0800

    Add files via upload

---
+++ timenuke.cs
@@ -0,0 +1,43 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        var rand = new Random();
+        DateTime Now = DateTime.Now;
+        string sourceDirectory = @"C:\Users\bchen\Desktop\MyConsoleApp";
+        var files = Directory.EnumerateFiles(sourceDirectory, "*.*", SearchOption.AllDirectories);
+        var directories = Directory.EnumerateDirectories(sourceDirectory, "*.*", SearchOption.AllDirectories);
+ 
+        foreach (string myFile in files){
+        // Calling the SetLastAccessTime() function
+        // to set last access date and time
+            try {
+                DateTime newDateTime = new DateTime(Now.Year, Now.Month, Now.Day, rand.Next(0, 23), rand.Next(0, 59), rand.Next(0, 59), rand.Next(0, 999));
+                File.SetLastAccessTime(myFile, newDateTime);
+                File.SetLastWriteTime(myFile, newDateTime);
+                File.SetCreationTime(myFile, newDateTime);
+            } catch {
+                continue;
+            }
+        }
+
+        foreach (string myDirectory in directories){
+        // Calling the SetLastAccessTime() function
+        // to set last access date and time
+            try {
+                DateTime newDateTime = new DateTime(Now.Year, Now.Month, Now.Day, rand.Next(0, 23), rand.Next(0, 59), rand.Next(0, 59), rand.Next(0, 999));
+                Directory.SetLastAccessTime(myDirectory, newDateTime);
+                Directory.SetLastWriteTime(myDirectory, newDateTime);
+                Directory.SetCreationTime(myDirectory, newDateTime);
+            } catch {
+                continue;
+            }
+        }
+
+    }
+}
